6daecffafe3b28faf5e381101782acc36fb9fd1f
michael emmons <memmons18@gmail.com>
2017-07-06 19:14:15 -0400

    near completed pages

---
+++ assets/javascript/profile.js
@@ -0,0 +1,8 @@
+$("#k-title").on("click", function(){
+
+	$(".links").toggleClass("links2");
+})
+
+// $(".glyphicon-menu-hamburger").on("click",function(){
+// 	$('.glyphicon-menu-hamburger').toggleClass('menu-hamburger-alt')
+// })
