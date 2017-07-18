db3a8e7bc0c219ca22abcd03456e2e89d0bb2e1e
michael emmons <memmons18@gmail.com>
2017-07-17 23:00:04 -0400

    media queries

--- assets/javascript/profile.js
+++ assets/javascript/profile.js
@@ -2,7 +2,9 @@ $("#k-title").on("click", function(){
 
 	$(".links").toggleClass("links2");
 })
-
+$("#menu-btn").on("click", function(){
+	$("")
+})
 // $(".glyphicon-menu-hamburger").on("click",function(){
 // 	$('.glyphicon-menu-hamburger').toggleClass('menu-hamburger-alt')
 // })
