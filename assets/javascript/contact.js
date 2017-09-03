1a9abbd323dec15c42b888bb32258e1be2455463
michael emmons <memmons18@gmail.com>
2017-09-03 11:42:05 -0400

    contact page info has been corrected and i am able to access info shared on it

---
+++ assets/javascript/contact.js
@@ -0,0 +1,51 @@
+// javascript for the contact page using firebase
+
+var config = {
+    apiKey: "AIzaSyDEbwvYvBS6Gf2ThzIz5qwjLw4WDRr1S0o",
+    authDomain: "portfolio-6f838.firebaseapp.com",
+    databaseURL: "https://portfolio-6f838.firebaseio.com",
+    projectId: "portfolio-6f838",
+    storageBucket: "portfolio-6f838.appspot.com",
+    messagingSenderId: "734399691310"
+  };
+  firebase.initializeApp(config);
+
+
+  var database= firebase.database();
+
+
+  $("#contact-form").submit(function(event){
+    event.preventDefault();
+        var name= $("#name").val().trim();
+        var email = $("#email").val().trim();
+        var comments = $("#message").val().trim();
+
+
+
+        var newPost= {    
+            comments: comments,
+            email: email,
+            name: name
+        };
+
+        database.ref().push(newPost);
+       
+        alert("Thank you " + name + " for submitting! I promise to respond in a timely matter")
+        $("#name").val("");
+        $("#email").val("");
+        $("#message").val("");
+
+  });
+
+  database.ref().on("child_added", function(childSnapShot, prevChildKey){
+      var name = childSnapShot.val().name;
+      var email = childSnapShot.val().email;
+      var comments = childSnapShot.val().comments;
+
+
+
+    
+
+      $(".display-messages").append("<tr><td>" + name + "<br>" + email + "<br>" + comments + "<br><br></tr></td>");
+  });
+
